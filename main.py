a1e99825bb0b9d9bbf7fd394845d25fcb0f6884e
Mark <mark.g.stockwell@gmail.com>
2022-04-24 20:09:53 -0400

    updated main.py

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ from google.oauth2 import service_account
 
 # logon and get credentials.
 if os.getenv('GAE_ENV', '').startswith('standard'):
-  # Production in the standard environment
-  client = bigquery.Client()
+    # Production in the standard environment
+    client = bigquery.Client()
 else:
-  # Local execution.
+    # Local execution.
     key_path = "c:\\tmp\\msd8654-498-dev-2d8f63cba08c.json"
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
+    client = bigquery.Client(credentials=credentials,
+                             project=credentials.project_id,)
 
 # Get data from BQ
+
 
 def query_bfro():
     #client = bigquery.Client()
@@ -48,9 +50,11 @@ def query_bfro():
     results = query_job.result()  # Waits for job to complete.
     return results
 
+
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
@@ -69,15 +73,16 @@ def index():
     # icon = folium.features.CustomIcon('https://icon-library.com/2993760.html', icon_size=(24, 24))
 
     # add markers for each sighting
-    
+
     for row in query_bfro():
         folium.Marker(
-        location=[row.latitude, row.longitude],
-        popup=row.link,
-        icon=folium.Icon(color=row.CENTROID_COLOR)
-    ).add_to(folium_map)
+            location=[row.latitude, row.longitude],
+            popup=row.link,
+            icon=folium.Icon(color=row.CENTROID_COLOR)
+        ).add_to(folium_map)
 
     return folium_map._repr_html_()
+
 
 @app.route('/map2')
 def index2():
@@ -86,49 +91,20 @@ def index2():
     # icon = folium.features.CustomIcon('https://icon-library.com/2993760.html', icon_size=(24, 24))
 
     # add markers for each sighting
-    
+
     for row in query_bfro():
         folium.Marker(
-        location=[row.latitude, row.longitude],
-        popup=row.link,
-        icon=folium.Icon(color=row.CENTROID_COLOR_CLIMATE)
-    ).add_to(folium_map)
+            location=[row.latitude, row.longitude],
+            popup=row.link,
+            icon=folium.Icon(color=row.CENTROID_COLOR_CLIMATE)
+        ).add_to(folium_map)
 
     return folium_map._repr_html_()
 
-@app.route('/map3')
-def index3():
-    start_coords = (36.00, -95.00)
-    folium_map = folium.Map(location=start_coords, zoom_start=5)
-    # icon = folium.features.CustomIcon('https://icon-library.com/2993760.html', icon_size=(24, 24))
-
-    # add markers for each sighting
-    
-    for row in query_bfro():
-        folium.Marker(
-        location=[row.latitude, row.longitude],
-        popup=row.link,
-        icon=folium.Icon(color=row.CENTROID_COLOR_GEO_CLIMATE)
-    ).add_to(folium_map)
-
-    return folium_map._repr_html_()
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
